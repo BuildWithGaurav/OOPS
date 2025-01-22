2303ef8f9452f793bff6a2d1930f28e92faedb36
Gaurav Singh <gs20040609@gmail.com>
2025-01-22 14:52:18 +0530

    Initial commit

---
+++ DisplayAddress.java
@@ -0,0 +1,15 @@
+public class DisplayAddress {
+    public static void main(String[] args) {
+        // Replace with your name and address
+        String name = "Gaurav Singh";
+        String addressLine1 = "123, Sec-5,  Avas Vikas Colony, Bodla-Sikandra ";
+        String addressLine2 = "MG Road, Andheri East";
+        String cityStatePin = "Mumbai, Maharashtra - 400069";
+
+        // Displaying the name and address
+        System.out.println(name);
+        System.out.println(addressLine1);
+        System.out.println(addressLine2);
+        System.out.println(cityStatePin);
+    }
+}
